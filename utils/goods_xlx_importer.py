726f7081d166a76cdb15a40d614b6afa8eabf673
radmirkus <radmirka745@gmail.com>
2021-10-28 20:41:34 +0300

    Fixed import for new xlx type

--- utils/goods_xlx_importer.py
+++ utils/goods_xlx_importer.py
@@ -12,15 +12,15 @@ def excel_row_to_item(row: tuple) -> Optional[GoodsItem]:
     name = row[0].strip() if row[0] else None
     description = row[1].strip() if row[1] else None
     category = row[2]
-    all_prices_str = " ".join(filter(lambda x: x is not None, row[3:]))
+    all_prices_str = " ".join(str(x) for x in filter(lambda x: x is not None, row[3:]))
     prices = list(map(float, re.findall(r'\d+', all_prices_str)))
     # prices = list(map(
     #     lambda p: float(p.replace("р", "").replace("Р", "").strip()),
     #     filter(lambda p: p is not None, row[3:])
     # ))
     # price = min(prices) if prices else None  # min price
-    price = sum(prices)/len(prices) if prices else None  # mean price
-    if all(x is None for x in [name, description, category, price]):  # все поля пустые
+    price = round(sum(prices)/len(prices), 2) if prices else None  # mean price
+    if all(x is None for x in [name, description, price]):  # все поля пустые
         return None
     item = GoodsItem(
         name=name,
@@ -41,5 +41,15 @@ def import_goods_from_xlx(goods_xlsx: BytesIO):
         item = excel_row_to_item(row)
         if item is None:
             continue
+        # print(item.to_dict())
         db.add_goods_item(item)
         # print(item.to_dict())
+
+
+# if __name__ == "__main__":
+#     # for test purposes
+#     test_file = "Kazan_v1.xlsx"
+#     with open(test_file, 'rb') as f:
+#         buf = BytesIO(f.read())
+#         import_goods_from_xlx(buf)
+
